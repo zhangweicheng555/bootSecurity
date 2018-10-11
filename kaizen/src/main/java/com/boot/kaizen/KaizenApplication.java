defe6ed08ac0ca32e1a2f1f9280a2e62f6bc05d6
unknown <a-zhangweicheng@L-080320-1.kuandeng.com>
2018-10-11 20:58:32 +0800

    熙增工作流配置

--- kaizen/src/main/java/com/boot/kaizen/KaizenApplication.java
+++ kaizen/src/main/java/com/boot/kaizen/KaizenApplication.java
@@ -13,5 +13,8 @@ public class KaizenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KaizenApplication.class, args);
+		
 	}
+	
+	
 }
---
+++ kaizen/src/main/java/com/boot/kaizen/config/ActivitiConfig.java
@@ -0,0 +1,75 @@
+package com.boot.kaizen.config;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+/**
+ * 工作流配置
+ * 
+ * @author a-zhangweicheng
+ */
+@Configuration
+public class ActivitiConfig {
+	 
+	    @Bean
+	    public ProcessEngine processEngine(DataSourceTransactionManager transactionManager, DataSource dataSource) throws IOException {
+	        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+	        //自动部署已有的流程文件
+	        Resource[] resources = new PathMatchingResourcePatternResolver().getResources(ResourceLoader.CLASSPATH_URL_PREFIX + "processes/*.bpmn");
+	        configuration.setTransactionManager(transactionManager);
+	        configuration.setDataSource(dataSource);
+	        configuration.setDatabaseSchemaUpdate("true");
+	        configuration.setDeploymentResources(resources);
+	        configuration.setDbIdentityUsed(false);
+	        configuration.setAsyncExecutorActivate(false);
+	        return configuration.buildProcessEngine();
+	    }
+	 
+	    @Bean
+	    public RepositoryService repositoryService(ProcessEngine processEngine) {
+	        return processEngine.getRepositoryService();
+	    }
+	 
+	    @Bean
+	    public RuntimeService runtimeService(ProcessEngine processEngine) {
+	        return processEngine.getRuntimeService();
+	    }
+	 
+	    @Bean
+	    public TaskService taskService(ProcessEngine processEngine) {
+	        return processEngine.getTaskService();
+	    }
+	 
+	    @Bean
+	    public HistoryService historyService(ProcessEngine processEngine) {
+	        return processEngine.getHistoryService();
+	    }
+	 
+	    @Bean
+	    public ManagementService managementService(ProcessEngine processEngine) {
+	        return processEngine.getManagementService();
+	    }
+	 
+	    @Bean
+	    public IdentityService identityService(ProcessEngine processEngine) {
+	        return processEngine.getIdentityService();
+	    }
+	 
+
+}
