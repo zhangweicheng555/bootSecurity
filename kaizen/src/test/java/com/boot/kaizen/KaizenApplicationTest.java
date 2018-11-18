37fd525888f7d26d49d086ef287ed10b82c10fd3
DESKTOP-LEDT8G5\dell <19348243@qq.com>
2018-11-18 18:30:33 +0800

    no message

--- kaizen/src/test/java/com/boot/kaizen/KaizenApplicationTest.java
+++ kaizen/src/test/java/com/boot/kaizen/KaizenApplicationTest.java
@@ -1,15 +1,11 @@
 package com.boot.kaizen;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.boot.kaizen.activiti.service.Activitiservice;
 
 /**
  * 测试类
@@ -20,12 +16,11 @@ import com.boot.kaizen.activiti.service.Activitiservice;
 @RunWith(SpringRunner.class)
 public class KaizenApplicationTest {
 
-	@Autowired
-	private Activitiservice activitiservice;
+	
 	
 	@Test
 	public void Test() throws InterruptedException, ExecutionException {
-		activitiservice.deleteProcessByDefinationId("LteConfig:1:35007", true);
+		
 	}
 
 }
