604415b0ecd0cf39d8add475f4af6d3ab268167e
unknown <a-zhangweicheng@L-080320-1.kuandeng.com>
2018-12-07 19:37:45 +0800

    完善

--- kaizen/src/main/java/com/boot/kaizen/_enum/Constant.java
+++ kaizen/src/main/java/com/boot/kaizen/_enum/Constant.java
@@ -2,10 +2,10 @@ package com.boot.kaizen._enum;
 
 public class Constant {
 
-	/**系统管理员*/
-	public static final Long  USER_ID=10L;
-	public static final Long  USER_ID_MASTER=8713L;
-	
-	/**系统项目*/
-	public static final Long  SYSTEM_ID_PROJECT=9L;
+	/** 系统管理员 */
+	public static final Long USER_ID = 10L;
+	public static final Long USER_ID_MASTER = 8713L;
+
+	/** 系统项目 */
+	public static final Long SYSTEM_ID_PROJECT = 9L;
 }
